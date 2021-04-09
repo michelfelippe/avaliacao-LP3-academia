ae022abac199be6890f5c542260ce035cac9d420
michelfelippe <63324004+michelfelippe@users.noreply.github.com>
2021-04-09 15:21:28 -0300

    v0.2 - Add Exercicios

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ var app = angular.module('App',['ngRoute','controllers']);
 app.config(['$routeProvider','$locationProvider', function($routeProvider,$locationProvider){
 
 	$routeProvider.when('/',{templateUrl: 'partials/main.html',controller: 'MainCtrl'}).
-	when('/listacliente',{templateUrl: 'partials/listacliente.html',controller: 'ListaCtrl'}).
+	when('/listacliente',{templateUrl: 'partials/listacliente.html',controller: 'ListaClienteCtrl'}).
 	when('/novocliente',{templateUrl:'partials/novocliente.html',controller:'ClienteCtrl'}).
 	when('/addcliente/:nomecliente/:cpf/:email/:idade/:endereco/:uf',{templateUrl:'partials/addcliente.html',controller:'AddClienteCtrl'});
+
+	$routeProvider.when('/',{templateUrl: 'partials/main.html',controller: 'MainCtrl'}).
+	when('/listaexercicios',{templateUrl: 'partials/listaexercicios.html',controller: 'ListaExercicioCtrl'}).
+	when('/novoexercicio',{templateUrl:'partials/novoexercicio.html',controller:'ExercicioCtrl'}).
+	when('/addexercicio/:nomeexercicio/:tipo/:tempo/:calorias/',{templateUrl:'partials/addexercicio.html',controller:'AddExercicioCtrl'});
 	
 	$locationProvider.html5Mode(false).hashPrefix('!');
 
--- controllers.js
+++ controllers.js
@@ -11,8 +11,13 @@ appControllers.controller('ListaCtrl',['$scope',function($scope){
 appControllers.controller('ClienteCtrl',['$scope','$location',function($scope,$location){
 	$scope.enviar=function(){
 		$location.path('/addcliente/'+$scope.nomecliente+'/'+$scope.cpf+'/'+$scope.idade+'/'+$scope.email+'/'+$scope.endereco+'/'+$scope.uf);
+}},
+
+		appControllers.controller('ExercicioCtrl',['$scope','$location',function($scope,$location){
+			$scope.enviar=function(){
+				$location.path('/addexercicio/'+$scope.nomeexercicio+'/'+$scope.tipo+'/'+$scope.tempo+'/'+$scope.calorias);
 	};
-}]);
+}]),
 
 appControllers.controller('AddClienteCtrl',['$scope','$routeParams',function($scope,$routeParams){
 
@@ -22,7 +27,14 @@ appControllers.controller('AddClienteCtrl',['$scope','$routeParams',function($sc
 	$scope.nidade = $routeParams.idade;
 	$scope.nendereco = $routeParams.endereco;
 	$scope.nuf = $routeParams.uf;
+}]),
 	
-	console.log('Cheguei até aqui!');
+	appControllers.controller('AddExercicioCtrl',['$scope','$routeParams',function($scope,$routeParams){
 	
-}]);
+		$scope.nnomeexercicio = $routeParams.nomeexercicio;
+		$scope.ntipo = $routeParams.tipo;
+		$scope.ntempo = $routeParams.tempo;
+		$scope.ncalorias = $routeParams.calorias;
+	
+	console.log('Cheguei até aqui! Exercicio!!!');
+	}])]);
